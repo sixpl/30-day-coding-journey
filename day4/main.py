6049450b65fb76bdfa90a6ba801e51e401880377
sixpl <saket@sixpl.com>
2026-03-08 03:08:39 +0530

    Connect fastAPI to Render Postgres

--- day4/main.py
+++ day4/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-# import psycopg2
+import os
+import psycopg2
 
 app = FastAPI()
+
+def get_connection():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 # conn = psycopg2.connect(
 #     dbname = "calculator_db",
@@ -22,6 +26,25 @@ app = FastAPI()
 
 # conn.commit()
 
+def create_table():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS calculations (
+                id SERIAL PRIMARY KEY,
+                a INTEGER,
+                b INTEGER,
+                result INTEGER
+                )
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+create_table()
+
 @app.get("/")
 def home():
     return{"message": "App is running!"}
@@ -33,6 +56,17 @@ def say_hello():
 @app.get("/add")
 def sum_cal(a:int, b:int):
     result = a + b
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO calculations (a, b, result) VALUES (%s, %s, %s)",
+        (a, b, result)
+    )
+
+    conn.commit()
+    cur.close(conn.close())
+
     return{"Addition is": result }
 
 # @app.get("/add")
